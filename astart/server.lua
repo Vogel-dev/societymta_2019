625d7be2c797f948d5ec032ab131f858f67836f6
Vogel-dev <60361545+Vogel-dev@users.noreply.github.com>
2020-02-02 01:43:40 +0100

    Add files via upload

---
+++ astart/server.lua
@@ -0,0 +1,67 @@
+﻿--[[
+@Author: Vogel
+@Copyright: Vogel / Society MTA // 2019-2020
+@Pierwotne prawo do użytku tego skryptu ma TYLKO autor i serwery otrzymujące zgodę na jego użytkowanie przez autora
+@Obowiązuje całkowity zakaz rozpowszechniania skryptów, zmiany autora, edycji skryptów bez zgody autora
+@Nie masz prawa użytkować tego skryptu bez mojej zgody.
+]]--
+
+addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+	if string.find(getResourceName(v), "smta_") or string.find(getResourceName(v), "smta_") then
+        stopResource(v)
+        outputDebugString(getResourceName(v).." stopped.")
+	end
+    end
+outputChatBox("SMTA / Skrypty zostały wyłączone", getRootElement())
+end)
+
+
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+    if string.find(getResourceName(v), "smta_") or string.find(getResourceName(v), "smta_") then
+        startResource(v)
+        outputDebugString(getResourceName(v).." started.")
+        end
+    end
+end)
+
+addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+	if string.find(getResourceName(v), "maps") or string.find(getResourceName(v), "maps_") then
+        stopResource(v)
+        outputDebugString(getResourceName(v).." stopped.")
+	end
+    end
+outputChatBox("MAPS / Skrypty zostały wyłączone", getRootElement())
+end)
+
+
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+    if string.find(getResourceName(v), "maps") or string.find(getResourceName(v), "maps_") then
+        startResource(v)
+        outputDebugString(getResourceName(v).." started.")
+        end
+    end
+end)
+
+addEventHandler("onResourceStop", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+	if string.find(getResourceName(v), "mod") or string.find(getResourceName(v), "mod_") then
+        stopResource(v)
+        outputDebugString(getResourceName(v).." stopped.")
+	end
+    end
+outputChatBox("MOD / Skrypty zostały wyłączone", getRootElement())
+end)
+
+
+addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
+    for k,v in ipairs(getResources()) do
+    if string.find(getResourceName(v), "mod") or string.find(getResourceName(v), "mod_") then
+        startResource(v)
+        outputDebugString(getResourceName(v).." started.")
+        end
+    end
+end)
